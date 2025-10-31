e2a95f2051a7e4e02d05ce42beab9afb4d7dca18
Ken <githubdotcom20250603_1349@merchnt.online>
2025-10-30 18:46:12 -0700

    MSSQL install and connection code

--- server/server.js
+++ server/server.js
@@ -5,13 +5,28 @@ import url from 'url'
 import { requestScryfall } from './scripts/main.js'
 import express from 'express'
 import sql from 'mssql'
+import { sqlConfig } from './sqlConfig.js'
 
 const PORT = process.env.PORT;
+const DB_NAME = process.env.DB_NAME;
+const DB_PWD = process.env.DB_PWD;
 const __dirname = import.meta.dirname;
 const __prevDir = path.join(__dirname, '..');
 const __public = path.join(__prevDir, '/', 'public');
 const server = http.createServer((req, res) => {});
 const app = express();
+
+// MSSQL connection
+(async () => {
+ try {
+  // make sure that any items are correctly URL encoded in the connection string
+  await sql.connect(sqlConfig)
+  const result = await sql.query`select * from mytable where id = ${value}`
+  console.dir(result)
+ } catch (err) {
+  // ... error checks
+ }
+})()
 
 // Express middleware
 app.use(express.json());
